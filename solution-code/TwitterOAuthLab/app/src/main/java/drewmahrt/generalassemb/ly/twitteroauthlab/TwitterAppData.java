98ed7efd4adbbd3e7b760a145c6acc28141fdf59
charlesdrews <charlesdrews@gmail.com>
2016-11-18 12:27:00 -0500

    Update solution code

--- solution-code/TwitterOAuthLab/app/src/main/java/drewmahrt/generalassemb/ly/twitteroauthlab/TwitterAppData.java
+++ solution-code/TwitterOAuthLab/app/src/main/java/drewmahrt/generalassemb/ly/twitteroauthlab/TwitterAppData.java
@@ -5,7 +5,7 @@ package drewmahrt.generalassemb.ly.twitteroauthlab;
  */
 public class TwitterAppData {
     //TODO: put your consumer key and consumer secret here
-    public final static String CONSUMER_KEY = "dSqlaKXzxGQ5qLS4k53jcjmVG";
-    public final static String CONSUMER_SECRET = "EC67DtX7q5Q20bkB966pzfzNbkfbfi7aYG85yOq1ryNXeXCRwd";
+    public final static String CONSUMER_KEY = "YOUR CONSUMER KEY GOES HERE";
+    public final static String CONSUMER_SECRET = "YOUR CONSUMER SECRET GOES HERE";
     public final static String BASE_URL = "https://api.twitter.com";
 }
